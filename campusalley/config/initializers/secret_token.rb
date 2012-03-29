193faae232c90eb595614087fec7d257fa9f699e
CampusAlley <developer.campusalley@gmail.com>
2012-03-28 21:54:15 -0400

    Initial rails

---
+++ campusalley/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campusalley::Application.config.secret_token = 'ad6280a5f45718ccd0d34d04ecaec0780ff6d4904f502f7a2bfe19dd32bb07b77f9b587be1c5897d6e6acad065f4ea2e9f4631b2713e686b916bdf200077e95a'
